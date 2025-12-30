31a574a6e0cd6300d8c7e81270d7985e8b556443
John03ran <John03ran@users.noreply.github.com>
2025-12-31 04:59:44 +0800

    添加了dropout和Fusion Layer到my_model.py中，新增了pyproject.toml以管理依赖。此外，更新了.gitignore以忽略Log目录。

--- Model/my_model.py
+++ Model/my_model.py
@@ -28,6 +28,17 @@ class Model(torch.nn.Module):
         self.r = config['r']
         self.model_path = config['model_path']
 
+        # Cache for optimization
+        self.A_sparse = None
+        self.norm_A_sq = None
+        self.norm_X_sq = None
+        self.D_vec = None
+
+        # Optimization: Add Dropout and Fusion Layer
+        dropout_rate = config.get('dropout', 0.5)
+        self.dropout = torch.nn.Dropout(dropout_rate)
+        self.fusion_layer = torch.nn.Linear(self.net_shape[-1] * 2, 1)
+
         self.fc1 = torch.nn.Linear(self.net_shape[-1], self.net_shape[1])
         self.fc2 = torch.nn.Linear(self.net_shape[1], self.net_shape[0])
 
@@ -73,10 +84,12 @@ class Model(torch.nn.Module):
 
     def projection1(self, z: torch.Tensor) -> torch.Tensor:
         z = F.elu(self.fc1(z.t()))
+        z = self.dropout(z)
         return self.fc2(z)
 
     def projection2(self, z: torch.Tensor) -> torch.Tensor:
         z = F.elu(self.fc3(z.t()))
+        z = self.dropout(z)
         return self.fc4(z)
 
     def sim(self, z1: torch.Tensor, z2: torch.Tensor):
@@ -91,16 +104,16 @@ class Model(torch.nn.Module):
 
         self.index_net = torch.argmax(self.V1, dim=0).long()
         O_net = F.one_hot(self.index_net, self.net_shape[-1]).float()
-        S_net = torch.mm(O_net, O_net.t())
-        # self.index_att = torch.argmax(self.V2, dim=0).long()
-        # O_att = F.one_hot(self.index_att, self.att_shape[-1]).float()
-        # S_att = torch.mm(O_att, O_att.t())
+        
+        # Optimization: Avoid constructing N x N S_net matrix
+        # S_net = torch.mm(O_net, O_net.t())
+        # refl_pos = refl_sim * S_net
+        
+        # Calculate row sums of (refl_sim * S_net) efficiently
+        # sum_j (R_ij * sum_k O_ik O_jk) = sum_k O_ik * (R @ O)_ik
+        refl_pos_sum = torch.sum(O_net * torch.mm(refl_sim, O_net), dim=1)
 
-        refl_pos = refl_sim * S_net
-        # between_pos = between_sim * S_net
-
-        # return -torch.log(between_sim.diag() / (refl_sim.sum(1) - refl_pos.sum(1) + between_sim.sum(1) - between_pos.sum(1) + between_sim.diag()))
-        return -torch.log((between_sim.diag()) / (refl_sim.sum(1) - refl_pos.sum(1) + between_sim.diag()))
+        return -torch.log((between_sim.diag()) / (refl_sim.sum(1) - refl_pos_sum + between_sim.diag()))
 
 
 
@@ -123,92 +136,96 @@ class Model(torch.nn.Module):
         # self.V2 = F.normalize(self.V['att' + str(len(self.att_shape) - 1)], p=2, dim=0)
         self.V1 = self.V['net' + str(len(self.net_shape) - 1)]
         self.V2 = self.V['att' + str(len(self.att_shape) - 1)]
-        # print('V1:', self.V1.shape)
-        # print('V2:', self.V2)
-        # return 0.5 * self.V1 + 0.5 * self.V2
-        return self.V1
+        
+        # Optimization: Attention-based Fusion
+        V1_t = self.V1.t()
+        V2_t = self.V2.t()
+        cat = torch.cat([V1_t, V2_t], dim=1)
+        weights = torch.sigmoid(self.fusion_layer(cat))
+        V_fused = weights * V1_t + (1 - weights) * V2_t
+        return V_fused.t()
 
     def loss(self, graph):
+        # Optimization: Precompute constants and use sparse operations
+        if self.A_sparse is None:
+            self.A_sparse = graph.A.to_sparse()
+            self.norm_A_sq = torch.square(torch.norm(graph.A))
+            self.norm_X_sq = torch.square(torch.norm(graph.X)) # graph.X is N x F
+            self.D_vec = torch.sum(graph.A, dim=1)
 
-        A = graph.A
-        X = graph.X.T
-        # D = torch.diag(torch.sum(graph.A, dim=1))
-        # D_S = torch.diag(torch.sum(graph.A, dim=1).pow(-0.5))
-        # L = torch.mm(torch.mm(D_S, D - graph.A), D_S)
-        # print(L)
-        L = torch.diag(torch.sum(graph.A, dim=1)) - A
-        # XS = torch.from_numpy(rbf_kernel(graph.X.cpu())).to(self.device)
-        # DX = torch.diag(torch.sum(XS, dim=1))
-        # DX_S = torch.diag(torch.sum(XS, dim=1).pow(-0.5))
-        # LX = torch.mm(torch.mm(DX_S, DX - XS), DX_S)
-        # print(LX)
-        # LX = F.normalize(DX - XS)
-
-        # reconstruction
-        P1 = torch.eye(self.net_input_dim, device=self.device)
-        # print(P1)
-        # print(self.U['net0'])
-        for i in range(len(self.net_shape)):
-            P1 = torch.mm(P1, self.U['net' + str(i)])
+        # reconstruction A
+        # P1 = U0 @ U1 ... @ V
+        P1_left = self.U['net0']
+        for i in range(1, len(self.net_shape)):
+            P1_left = torch.mm(P1_left, self.U['net' + str(i)])
+        
         i = len(self.net_shape) - 1
-        P1 = torch.mm(P1, self.V['net' + str(i)])
-        loss1 = torch.square(torch.norm(A - P1))
-        # print('done:loss1', loss1)
+        V_net = self.V['net' + str(i)]
+        
+        # ||A - UV||^2 = ||A||^2 + ||UV||^2 - 2 Tr(V A U)
+        # ||UV||^2 = Tr(V^T U^T U V) = Tr(V V^T U^T U)
+        norm_P1_sq = torch.trace(torch.mm(torch.mm(V_net, V_net.t()), torch.mm(P1_left.t(), P1_left)))
+        
+        # Tr(V A U) = Tr(V (A U))
+        # A is sparse. A @ U is dense (N x k)
+        AU = torch.sparse.mm(self.A_sparse, P1_left)
+        tr_VAU = torch.sum(V_net * AU.t())
+        
+        loss1 = self.norm_A_sq + norm_P1_sq - 2 * tr_VAU
 
-        P2 = torch.eye(self.att_input_dim, device=self.device)
-        for i in range(len(self.att_shape)):
-            P2 = torch.mm(P2, self.U['att' + str(i)])
+        # reconstruction X
+        # P2 = U0 @ U1 ... @ V
+        P2_left = self.U['att0']
+        for i in range(1, len(self.att_shape)):
+            P2_left = torch.mm(P2_left, self.U['att' + str(i)])
+            
         i = len(self.att_shape) - 1
-        P2 = torch.mm(P2, self.V['att' + str(i)])
-        loss2 = torch.square(torch.norm(X - P2))
-        # print('done:loss2', loss2)
+        V_att = self.V['att' + str(i)]
+        
+        # ||X - P2||^2. X is F x N (graph.X.T). P2 is F x N.
+        # P2 = P2_left @ V_att. P2_left (F x k), V_att (k x N).
+        norm_P2_sq = torch.trace(torch.mm(torch.mm(V_att, V_att.t()), torch.mm(P2_left.t(), P2_left)))
+        
+        # Tr(P2^T X) = Tr(V^T U^T X) = Tr(X V^T U^T) ? No.
+        # <X, P2> = Tr(X^T P2) = Tr(X^T U V) = Tr(V X^T U)
+        # X^T is N x F (graph.X). U is F x k.
+        # X^T @ U is N x k.
+        XTU = torch.mm(graph.X, P2_left)
+        tr_XP2 = torch.sum(V_att * XTU.t())
+        
+        loss2 = self.norm_X_sq + norm_P2_sq - 2 * tr_XP2
 
         # contrastive loss
         loss3 = self.contra_loss(self.V1, self.V2)
-        # print('done:loss3', loss3)
 
         # regularization loss
-        i = len(self.net_shape) - 1
-        # print(L)
-        # print(self.V['net' + str(i)])
-        # T = torch.mm(self.V['net' + str(i)], L)
-        M = torch.mm(torch.mm(self.V['net' + str(i)], L), self.V['net' + str(i)].t())
-        i = len(self.att_shape) - 1
-        # print(L)
-        # print(self.V['net' + str(i)])
-        # TX = torch.mm(self.V['att' + str(i)], L)
-        MX = torch.mm(torch.mm(self.V['att' + str(i)], L), self.V['att' + str(i)].t())
-        loss4 = torch.trace(M) + torch.trace(MX)
-        # loss4 = torch.trace(M)
-        # print('done:loss4', loss4)
+        # Trace(V L V^T) = Trace(V D V^T) - Trace(V A V^T)
+        # Trace(V D V^T) = sum_k D_k * ||V_k||^2
+        tr_VDVT_net = torch.sum(self.D_vec * torch.sum(V_net**2, dim=0))
+        # Trace(V A V^T) = Trace(V (A V^T))
+        AVT_net = torch.sparse.mm(self.A_sparse, V_net.t())
+        tr_VAVT_net = torch.sum(V_net * AVT_net.t())
+        loss4_net = tr_VDVT_net - tr_VAVT_net
+        
+        tr_VDVT_att = torch.sum(self.D_vec * torch.sum(V_att**2, dim=0))
+        AVT_att = torch.sparse.mm(self.A_sparse, V_att.t())
+        tr_VAVT_att = torch.sum(V_att * AVT_att.t())
+        loss4_att = tr_VDVT_att - tr_VAVT_att
+        
+        loss4 = loss4_net + loss4_att
 
         # nonnegative loss item
         loss5 = 0
         for i in range(len(self.net_shape)):
-            zero1 = torch.zeros_like(self.U['net' + str(i)])
-            X1 = torch.where(self.U['net' + str(i)] > 0, zero1, self.U['net' + str(i)])
-            loss5 = loss5 + torch.square(torch.norm(X1))
-        zero1 = torch.zeros_like(self.V['net' + str(i)])
-        X1 = torch.where(self.V['net' + str(i)] > 0, zero1, self.V['net' + str(i)])
-        loss5 = loss5 + torch.square(torch.norm(X1))
+            loss5 += torch.sum(torch.relu(-self.U['net' + str(i)])**2)
+        loss5 += torch.sum(torch.relu(-self.V['net' + str(i)])**2)
 
         for i in range(len(self.att_shape)):
-            zero2 = torch.zeros_like(self.U['att' + str(i)])
-            X2 = torch.where(self.U['att' + str(i)] > 0, zero2, self.U['att' + str(i)])
-            loss5 = loss5 + torch.square(torch.norm(X2))
+            loss5 += torch.sum(torch.relu(-self.U['att' + str(i)])**2)
         i = len(self.att_shape) - 1
-        zero2 = torch.zeros_like(self.V['att' + str(i)])
-        X2 = torch.where(self.V['att' + str(i)] > 0, zero2, self.V['att' + str(i)])
-        loss5 = loss5 + torch.square(torch.norm(X2))
-        # loss5 = -(torch.sum(X1) + torch.sum(X2))
-        # print('done:loss5', loss5)
+        loss5 += torch.sum(torch.relu(-self.V['att' + str(i)])**2)
 
         loss = self.rec*(loss1 + loss2) + self.conc*loss3 + self.r*loss4 + self.negc*loss5
-        # print('done:loss', loss)
-
-        # for i in range(len(self.net_shape)):
-        #     print(self. U['net' + str(i)].shape)
-
 
         return loss, loss1, loss2, loss3, loss4, loss5
 
